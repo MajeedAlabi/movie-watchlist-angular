2ab4219cbacc4038e3806581ab7b9d284fcb3ce0
MajeedAlabi <kehindealabi123@gmail.com>
2024-10-07 11:15:52 +0100

    updated code to the correct port

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,21 +23,21 @@ export class HomeComponent implements OnInit {
   }
 
   getTrendingMovies(){
-    this.http.get("http://localhost:49921/assets/data/trending-movies.json")
+    this.http.get("http://localhost:4200/assets/data/trending-movies.json")
     .subscribe((movies) => {
       this.trendingmovies = movies;
     }); 
   }
 
   getPopularMovies(){
-    this.http.get("http://localhost:49921/assets/data/popular-movies.json")
+    this.http.get("http://localhost:4200/assets/data/popular-movies.json")
     .subscribe((movies) => {
       this.popularmovies = movies;
     }); 
   }
 
   getTheatreMovies(){
-    this.http.get("http://localhost:49921/assets/data/theatre-movies.json")
+    this.http.get("http://localhost:4200/assets/data/theatre-movies.json")
     .subscribe((movies) => {
       this.theatremovies = movies;
     }); 
